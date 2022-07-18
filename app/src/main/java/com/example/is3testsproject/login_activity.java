4dc9305fd1b0bf18ba02be07b360879fe7b3dd72
Gabriela <64268942+gchipanap@users.noreply.github.com>
2022-07-18 02:28:14 -0500

    function buttons

--- app/src/main/java/com/example/is3testsproject/login_activity.java
+++ app/src/main/java/com/example/is3testsproject/login_activity.java
@@ -4,16 +4,42 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class login_activity extends Activity {
     /** Called when the activity is first created. */
+    TextView ingresar;
+    TextView registrarse_ek1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        ingresar = (TextView) findViewById(R.id.ingresar);
+
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_activity.this, home_activity.class));
+            }
+        });
+
+        registrarse_ek1 = (TextView) findViewById(R.id.registrarse_ek1);
+
+        registrarse_ek1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login_activity.this, register_activity.class));
+            }
+        });
+
     }
 
     public void onClickToHome(View v){
         startActivity(new Intent(login_activity.this, home_activity.class));
     }
+
+
+
 }
--- app/src/main/java/com/example/is3testsproject/tienda_activity.java
+++ app/src/main/java/com/example/is3testsproject/tienda_activity.java
@@ -1,6 +1,57 @@
 package com.example.is3testsproject;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class tienda_activity extends Activity {
+    TextView mi_actividad;
+    TextView my_profile;
+    TextView tienda_ek4;
+    TextView home_ek2;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        mi_actividad = (TextView) findViewById(R.id.mi_actividad);
+
+        mi_actividad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tienda_activity.this, mi_actividad_activity.class));
+            }
+        });
+
+        my_profile = (TextView) findViewById(R.id.my_profile);
+
+        my_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tienda_activity.this, mi_perfil_activity.class));
+            }
+        });
+
+        tienda_ek4 = (TextView) findViewById(R.id.tienda_ek4);
+
+        tienda_ek4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tienda_activity.this, tienda_activity.class));
+            }
+        });
+
+        home_ek2 = (TextView) findViewById(R.id.home_ek2);
+
+        home_ek2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(tienda_activity.this, home_activity.class));
+            }
+        });
+
+    }
 }
