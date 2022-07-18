473f88c508912c659f412707463477152aead2cf
Maycol <42551238+YoberM@users.noreply.github.com>
2022-07-18 00:04:11 -0500

    Add files via upload

---
+++ app/src/test/androidTest/java/com/example/is3testsproject/mi_perfil_activity.java
@@ -0,0 +1,158 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		mi_perfil
+	 *	@date 		1657068480249
+	 *	@title 		Final design
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.is3testsproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class mi_perfil_activity extends Activity {
+
+	
+	private View _bg__mi_perfil_ek2;
+	private TextView _10___34;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private ImageView vector_ek2;
+	private ImageView vector_ek3;
+	private ImageView vector_ek4;
+	private ImageView vector_ek5;
+	private TextView home;
+	private TextView mi_perfil_ek3;
+	private ImageView vector_ek6;
+	private TextView my_courses;
+	private ImageView vector_ek7;
+	private ImageView vector_ek8;
+	private TextView blogs;
+	private ImageView vector_ek9;
+	private ImageView vector_ek10;
+	private ImageView vector_ek11;
+	private ImageView vector_ek12;
+	private ImageView vector_ek13;
+	private ImageView vector_ek14;
+	private TextView mi_perfil_ek4;
+	private ImageView vector_ek15;
+	private ImageView vector_ek16;
+	private View ellipse_9;
+	private TextView shober_mendoza;
+	private TextView poroto_coin_gmail_com;
+	private TextView informacion_basica;
+	private ImageView vector_ek17;
+	private ImageView vector_ek18;
+	private ImageView vector_ek19;
+	private ImageView vector_ek20;
+	private ImageView vector_ek21;
+	private ImageView vector_ek22;
+	private ImageView vector_ek23;
+	private ImageView vector_ek24;
+	private TextView medios_de_transporte_usados;
+	private TextView _22_a_os__17_noviembre_1970_;
+	private TextView taxi;
+	private TextView combi;
+	private TextView mototaxi;
+	private TextView _70_00;
+	private TextView _170_00;
+	private TextView m;
+	private TextView edad;
+	private TextView peso;
+	private TextView altura;
+	private TextView sexo;
+	private ImageView vector_ek25;
+	private ImageView vector_ek26;
+	private TextView sign_out;
+	private ImageView vector_ek27;
+	private ImageView vector_ek28;
+	private TextView l;
+	private View line_4;
+	private View line_5;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mi_perfil);
+
+		
+		_bg__mi_perfil_ek2 = (View) findViewById(R.id._bg__mi_perfil_ek2);
+		_10___34 = (TextView) findViewById(R.id._10___34);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
+		home = (TextView) findViewById(R.id.home);
+		mi_perfil_ek3 = (TextView) findViewById(R.id.mi_perfil_ek3);
+		vector_ek6 = (ImageView) findViewById(R.id.vector_ek6);
+		my_courses = (TextView) findViewById(R.id.my_courses);
+		vector_ek7 = (ImageView) findViewById(R.id.vector_ek7);
+		vector_ek8 = (ImageView) findViewById(R.id.vector_ek8);
+		blogs = (TextView) findViewById(R.id.blogs);
+		vector_ek9 = (ImageView) findViewById(R.id.vector_ek9);
+		vector_ek10 = (ImageView) findViewById(R.id.vector_ek10);
+		vector_ek11 = (ImageView) findViewById(R.id.vector_ek11);
+		vector_ek12 = (ImageView) findViewById(R.id.vector_ek12);
+		vector_ek13 = (ImageView) findViewById(R.id.vector_ek13);
+		vector_ek14 = (ImageView) findViewById(R.id.vector_ek14);
+		mi_perfil_ek4 = (TextView) findViewById(R.id.mi_perfil_ek4);
+		vector_ek15 = (ImageView) findViewById(R.id.vector_ek15);
+		vector_ek16 = (ImageView) findViewById(R.id.vector_ek16);
+		ellipse_9 = (View) findViewById(R.id.ellipse_9);
+		shober_mendoza = (TextView) findViewById(R.id.shober_mendoza);
+		poroto_coin_gmail_com = (TextView) findViewById(R.id.poroto_coin_gmail_com);
+		informacion_basica = (TextView) findViewById(R.id.informacion_basica);
+		vector_ek17 = (ImageView) findViewById(R.id.vector_ek17);
+		vector_ek18 = (ImageView) findViewById(R.id.vector_ek18);
+		vector_ek19 = (ImageView) findViewById(R.id.vector_ek19);
+		vector_ek20 = (ImageView) findViewById(R.id.vector_ek20);
+		vector_ek21 = (ImageView) findViewById(R.id.vector_ek21);
+		vector_ek22 = (ImageView) findViewById(R.id.vector_ek22);
+		vector_ek23 = (ImageView) findViewById(R.id.vector_ek23);
+		vector_ek24 = (ImageView) findViewById(R.id.vector_ek24);
+		medios_de_transporte_usados = (TextView) findViewById(R.id.medios_de_transporte_usados);
+		_22_a_os__17_noviembre_1970_ = (TextView) findViewById(R.id._22_a_os__17_noviembre_1970_);
+		taxi = (TextView) findViewById(R.id.taxi);
+		combi = (TextView) findViewById(R.id.combi);
+		mototaxi = (TextView) findViewById(R.id.mototaxi);
+		_70_00 = (TextView) findViewById(R.id._70_00);
+		_170_00 = (TextView) findViewById(R.id._170_00);
+		m = (TextView) findViewById(R.id.m);
+		edad = (TextView) findViewById(R.id.edad);
+		peso = (TextView) findViewById(R.id.peso);
+		altura = (TextView) findViewById(R.id.altura);
+		sexo = (TextView) findViewById(R.id.sexo);
+		vector_ek25 = (ImageView) findViewById(R.id.vector_ek25);
+		vector_ek26 = (ImageView) findViewById(R.id.vector_ek26);
+		sign_out = (TextView) findViewById(R.id.sign_out);
+		vector_ek27 = (ImageView) findViewById(R.id.vector_ek27);
+		vector_ek28 = (ImageView) findViewById(R.id.vector_ek28);
+		l = (TextView) findViewById(R.id.l);
+		line_4 = (View) findViewById(R.id.line_4);
+		line_5 = (View) findViewById(R.id.line_5);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ app/src/test/androidTest/java/com/example/is3testsproject/tienda_activity.java
@@ -0,0 +1,171 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		mi_perfil
+	 *	@date 		1657068480249
+	 *	@title 		Final design
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.is3testsproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.content.Intent;
+
+public class tienda_activity extends Activity {
+
+	
+	private View _bg__tienda_ek2;
+	private TextView _10___34_ek1;
+	private ImageView vector_ek29;
+	private ImageView vector_ek30;
+	private ImageView vector_ek31;
+	private ImageView vector_ek32;
+	private ImageView vector_ek33;
+	private ImageView vector_ek34;
+	private ImageView vector_ek35;
+	private ImageView vector_ek36;
+	private TextView tienda_ek3;
+	private TextView _50;
+	private ImageView vector_ek37;
+	private TextView avatar_1;
+	private TextView sdsd;
+	private ImageView vector_ek38;
+	private TextView avatar_2;
+	private TextView _10;
+	private ImageView vector_ek39;
+	private View unsplash_zwd435_ewb4;
+	private TextView avatar_3;
+	private TextView _6;
+	private ImageView vector_ek40;
+	private ImageView image_7;
+	private TextView avatar_4;
+	private TextView _15;
+	private ImageView vector_ek41;
+	private ImageView unsplash_f57lx37dcm4;
+	private TextView walpaper_premium;
+	private TextView _30;
+	private ImageView vector_ek42;
+	private TextView avatar_clasico;
+	private TextView _3;
+	private ImageView vector_ek43;
+	private ImageView image_5;
+	private TextView avatar_gratis;
+	private TextView _0;
+	private ImageView vector_ek44;
+	private ImageView image_6;
+	private View unsplash_gpnohig3lsu;
+	private TextView machine_learning_course;
+	private TextView _4_5;
+	private ImageView vector_ek45;
+	private TextView _10_5k_learners;
+	private TextView home_ek1;
+	private ImageView vector_ek46;
+	private TextView tienda_ek4;
+	private ImageView vector_ek47;
+	private ImageView vector_ek48;
+	private ImageView vector_ek49;
+	private TextView my_profile;
+	private TextView mi_actividad;
+	private ImageView vector_ek50;
+	private ImageView vector_ek51;
+	private ImageView _image_3;
+	private ImageView image_4;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tienda);
+
+		
+		_bg__tienda_ek2 = (View) findViewById(R.id._bg__tienda_ek2);
+		_10___34_ek1 = (TextView) findViewById(R.id._10___34_ek1);
+		vector_ek29 = (ImageView) findViewById(R.id.vector_ek29);
+		vector_ek30 = (ImageView) findViewById(R.id.vector_ek30);
+		vector_ek31 = (ImageView) findViewById(R.id.vector_ek31);
+		vector_ek32 = (ImageView) findViewById(R.id.vector_ek32);
+		vector_ek33 = (ImageView) findViewById(R.id.vector_ek33);
+		vector_ek34 = (ImageView) findViewById(R.id.vector_ek34);
+		vector_ek35 = (ImageView) findViewById(R.id.vector_ek35);
+		vector_ek36 = (ImageView) findViewById(R.id.vector_ek36);
+		tienda_ek3 = (TextView) findViewById(R.id.tienda_ek3);
+		_50 = (TextView) findViewById(R.id._50);
+		vector_ek37 = (ImageView) findViewById(R.id.vector_ek37);
+		avatar_1 = (TextView) findViewById(R.id.avatar_1);
+		sdsd = (TextView) findViewById(R.id.sdsd);
+		vector_ek38 = (ImageView) findViewById(R.id.vector_ek38);
+		avatar_2 = (TextView) findViewById(R.id.avatar_2);
+		_10 = (TextView) findViewById(R.id._10);
+		vector_ek39 = (ImageView) findViewById(R.id.vector_ek39);
+		unsplash_zwd435_ewb4 = (View) findViewById(R.id.unsplash_zwd435_ewb4);
+		avatar_3 = (TextView) findViewById(R.id.avatar_3);
+		_6 = (TextView) findViewById(R.id._6);
+		vector_ek40 = (ImageView) findViewById(R.id.vector_ek40);
+		image_7 = (ImageView) findViewById(R.id.image_7);
+		avatar_4 = (TextView) findViewById(R.id.avatar_4);
+		_15 = (TextView) findViewById(R.id._15);
+		vector_ek41 = (ImageView) findViewById(R.id.vector_ek41);
+		unsplash_f57lx37dcm4 = (ImageView) findViewById(R.id.unsplash_f57lx37dcm4);
+		walpaper_premium = (TextView) findViewById(R.id.walpaper_premium);
+		_30 = (TextView) findViewById(R.id._30);
+		vector_ek42 = (ImageView) findViewById(R.id.vector_ek42);
+		avatar_clasico = (TextView) findViewById(R.id.avatar_clasico);
+		_3 = (TextView) findViewById(R.id._3);
+		vector_ek43 = (ImageView) findViewById(R.id.vector_ek43);
+		image_5 = (ImageView) findViewById(R.id.image_5);
+		avatar_gratis = (TextView) findViewById(R.id.avatar_gratis);
+		_0 = (TextView) findViewById(R.id._0);
+		vector_ek44 = (ImageView) findViewById(R.id.vector_ek44);
+		image_6 = (ImageView) findViewById(R.id.image_6);
+		unsplash_gpnohig3lsu = (View) findViewById(R.id.unsplash_gpnohig3lsu);
+		machine_learning_course = (TextView) findViewById(R.id.machine_learning_course);
+		_4_5 = (TextView) findViewById(R.id._4_5);
+		vector_ek45 = (ImageView) findViewById(R.id.vector_ek45);
+		_10_5k_learners = (TextView) findViewById(R.id._10_5k_learners);
+		home_ek1 = (TextView) findViewById(R.id.home_ek1);
+		vector_ek46 = (ImageView) findViewById(R.id.vector_ek46);
+		tienda_ek4 = (TextView) findViewById(R.id.tienda_ek4);
+		vector_ek47 = (ImageView) findViewById(R.id.vector_ek47);
+		vector_ek48 = (ImageView) findViewById(R.id.vector_ek48);
+		vector_ek49 = (ImageView) findViewById(R.id.vector_ek49);
+		my_profile = (TextView) findViewById(R.id.my_profile);
+		mi_actividad = (TextView) findViewById(R.id.mi_actividad);
+		vector_ek50 = (ImageView) findViewById(R.id.vector_ek50);
+		vector_ek51 = (ImageView) findViewById(R.id.vector_ek51);
+		_image_3 = (ImageView) findViewById(R.id._image_3);
+		image_4 = (ImageView) findViewById(R.id.image_4);
+	
+		
+		_image_3.setOnClickListener(new View.OnClickListener() {
+		
+			public void onClick(View v) {
+				
+				Intent nextScreen = new Intent(getApplicationContext(), item_comprado_activity.class);
+				startActivity(nextScreen);
+			
+		
+			}
+		});
+		
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
