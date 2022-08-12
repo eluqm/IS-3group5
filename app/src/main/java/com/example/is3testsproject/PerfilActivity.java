24fff59ee66c821c8f0d98f5c0cebdd2e927928b
Joselyn7 <64213758+Joselyn7@users.noreply.github.com>
2022-08-12 16:06:54 -0500

    Se agrego dependencias para Perfil

--- app/src/main/java/com/example/is3testsproject/PerfilActivity.java
+++ app/src/main/java/com/example/is3testsproject/PerfilActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.is3testsproject.Models.Users;
 import com.example.is3testsproject.databinding.ActivityPerfilBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +18,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 
